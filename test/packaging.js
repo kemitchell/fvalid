bc9015e79c54aea179ef3786143d50c3c3c0cd6c
Kyle Mitchell <kyle@kemitchell.com>
2014-12-11 19:30:47 -0800

    condensed packaging test suite

--- test/packaging.js
+++ test/packaging.js
@@ -11,32 +11,20 @@
   var library = require(path.join(__dirname, '..', commonjs.main));
 
   var itIsTheSame = function(property) {
-    it('is the same for NPM and Bower', function() {
+    it(property + ' is the same for NPM and Bower', function() {
       commonjs[property].should.eql(bower[property]);
     });
   };
 
   var itMatchesModule = function(property) {
-    it('is the same for NPM on export', function() {
+    it(property + ' is the same for NPM on export', function() {
       library[property].should.eql(commonjs[property]);
     });
   };
 
-  describe('Package', function() {
-    describe('version', function() {
-      it('is a valid semantic version', function() {
-        semver.valid(commonjs.version).should.not.equal(null);
-      });
-
-      itIsTheSame('version');
-
-      itMatchesModule('version');
-    });
-
-    describe('name', function() {
-      itIsTheSame('name');
-
-      itMatchesModule('name');
+  describe('Packaging', function() {
+    it('version is a valid semantic version', function() {
+      semver.valid(commonjs.version).should.not.equal(null);
     });
 
     [
@@ -45,11 +33,14 @@
       'homepage',
       'keywords',
       'license',
-      'repository'
+      'name',
+      'repository',
+      'version'
     ].forEach(function(property) {
-      describe(property, function() {
-        itIsTheSame(property);
-      });
+      itIsTheSame(property);
     });
+
+    itMatchesModule('version');
+    itMatchesModule('name');
   });
 })();
